a0512fe621e7215785fe93e42f7a646369af17d3
johnlloyd <yukimiranda26@gmail.com>
2023-04-12 00:45:00 +0800

    sing up for client

--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -8,15 +8,19 @@ import { Database,set,ref,onValue } from '@angular/fire/database';
   styleUrls: ['./login-signup.component.css']
 })
 export class LoginSignupComponent implements OnInit {
-
-  checker = "";
   uuid = "";
-
+  checker = "";
+  checker1 = "";
+  password = "";
+  cpassword = "";
 
   constructor(private router:Router,public database:Database) { }
 
   ngOnInit(): void {
   }
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 login(value:any){
   sessionStorage.setItem('id',value.username);
   this.router.navigate(['/home'])
@@ -25,46 +29,59 @@ login(value:any){
   });
 
 }
-pang=false;
-signup(value:any){
-  const starCountRef = ref(this.database, 'accounts/' + value.email);
+
+  async signup(value:any){
+
+  this.password = value.password;
+  this.cpassword = value.cpassword;
+  const starCountRef = ref(this.database, 'staff/' + value.username);
   onValue(starCountRef, (snapshot) => {
-   const db = snapshot.val();  
-this.checker = db.username
+   const ad = snapshot.val();  
+this.checker = ad.username
 
    }); 
 
-    
-   if (this.pang == true){
+    const starCountRef1 = ref(this.database, 'client/' + value.username);
+    onValue(starCountRef1, (snapshot) => {
+     const cli = snapshot.val();  
+  this.checker1 = cli.username
 
-    alert('Fill the form ');
+     }); 
+   if (this.password != this.cpassword){
+
+    alert('Password Dint match ');
    }else{
-    if(this.checker == value.email){
+    await this.delay(1000);
+    if(this.checker == value.username || this.checker1 == value.username ){
      alert('user email already exist!'); 
+     console.log(this.checker," and ",this.checker1)
     }
 
       
     else {
+    
+       console.log("test1")
+       this.uuid = "client" +Math.floor(100000 + Math.random() * 900000);
+   set(ref(this.database, 'client/' + value.username), {
+       id: this.uuid,
+       username: value.username,
+       password: value.password,
+       firstname: value.fname,
+       lastname: value.lname,
+       Email: value.email,
+       gender: value.set,
+      address: value.address,
+       phonenumber: value.phonenumber,
+       activity: false
 
-      this.uuid = "staff" +Math.floor(100000 + Math.random() * 900000);
-  set(ref(this.database, 'staff/' + value.username), {
-      id: this.uuid,
-      username: value.username,
-      password: value.password,
-      firstname: value.fname,
-      lastname: value.lname,
-      Email: "",
-      gender: "male",
-      phonenumber: value.phonenumber,
-      jobtitle: "owner",
-      admin: true,
-      rootadmin: true,
-      activity: false
 
-
-     }); 
-     alert('account created!');
-     this.router.navigate(['/login'])
+      }); 
+      this.checker = "";
+      this.checker1 = "";
+      this.password = "";
+      this.cpassword = "";
+      alert('account created!');
+      this.router.navigate(['/login'])
     }
    }
    
