7836060a6e9aa408c40f75666f1fbba939dc6b61
johnlloyd <yukimiranda26@gmail.com>
2023-04-29 21:24:27 +0800

    fix some in home page and hosting online

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  username = sessionStorage.getItem('id');
 
   constructor() {
-  
+
    }
 
   ngOnInit(): void {
