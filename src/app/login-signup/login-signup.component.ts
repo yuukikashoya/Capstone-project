f92f7822270c5a437745978b57121073a9095d8c
johnlloyd <yukimiranda26@gmail.com>
2023-05-04 03:10:29 +0800

    sign up design and  funtion complete fix desgin nalng

--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -18,7 +18,12 @@ export class LoginSignupComponent implements OnInit {
   staffdata="";
 clientname = "";
 staffname="";
+log: boolean = false;
 
+signlogin() {
+  this.log = !this.log;
+
+}
 
   showpassword() {
     this.show = !this.show;
@@ -146,8 +151,10 @@ this.checker = ad.username
       this.checker1 = "";
       this.password = "";
       this.cpassword = "";
+      this.log = !this.log;
       alert('account created!');
-      this.router.navigate(['/login'])
+ 
+
     }
    }
    
