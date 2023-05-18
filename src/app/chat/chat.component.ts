5577202c10198b19e49275c11e4c9ae50f36810e
johnlloyd <yukimiranda26@gmail.com>
2023-05-19 00:56:18 +0800

    chat in the user side

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Database, ref, set } from '@angular/fire/database';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  chats!: Observable<any[]>;
+  iid= sessionStorage.getItem('id');
+  admin = false
+  constructor(public database:Database,private FireDb: AngularFireDatabase) {
+    this.chats = FireDb.list('/chat/'+ this.iid).valueChanges();
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
   }
+  chatid =1
+  chat = ""
+chatcount=0
 
+sendchat(value:any){
+
+  if(value.chat == null || value.chat == "" || value.chat == undefined){
+  alert("wala")
+ }else{
+  
+  const date = new Date();
+  
+  
+  set(ref(this.database, 'chat/' + this.iid + '/'+date), {
+  usderid: this.iid,
+  time:  date,
+  chat: value.chat,
+  admin: this.admin
+
+
+
+ });
+this.chat = ""
+
+ }
+}
 }
