3ac56e85271bc1442042f5ea644dcd01559220d1
johnlloyd <yukimiranda26@gmail.com>
2023-07-18 16:02:57 +0800

    session blocking hackker to access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Capstone';
+
+  constructor(private router:Router){
+    const sessionValue = sessionStorage.getItem('id');
+
+    if (sessionValue) {
+
+    } else {
+      this.router.navigate(['/sign'])
+    }
+
+  }
 }
--- src/app/manage-sales/manage-sales.component.ts
+++ src/app/manage-sales/manage-sales.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-sales',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageSalesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router) {
+
+    const sessionValue = sessionStorage.getItem('type');
+   
+    if (sessionValue == "1" ) {
+    
+    } else {
+      this.router.navigate(['/sign'])
+    }
+   }
 
   ngOnInit(): void {
   }
