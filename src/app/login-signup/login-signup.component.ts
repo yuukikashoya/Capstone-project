a600e5943a144c4e939118ffe71be1f667488fb5
johnlloyd <yukimiranda26@gmail.com>
2023-08-28 00:07:36 +0800

    bug fix

--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -26,13 +26,35 @@ fname=""
 lname=""
 username=""
 email=""
-phonenumber=""
+phonenumber="9"
 
 address=""
 ccpassword=""
 cccpassword=""
 set=""
 
+
+
+
+validateInput(event: KeyboardEvent) {
+  const inputChar = event.key;
+
+  // Only allow digits and backspace
+  if (!/^[0-9]$/.test(inputChar) && inputChar !== 'Backspace') {
+    event.preventDefault();
+
+  }
+}
+handleInput(event: any) {
+  const inputValue = event.target.value;
+
+  // Ensure '9' remains at the beginning
+  if (inputValue === '' || !inputValue.startsWith('9')) {
+    this.phonenumber = '9' + inputValue.replace(/[^0-9]/g, ''); // Remove non-digit characters
+  } else {
+    this.phonenumber = inputValue.replace(/[^0-9]/g, ''); // Remove non-digit characters
+  }
+}
 log: boolean = false;
 
 signlogin() {
