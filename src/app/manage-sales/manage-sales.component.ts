c403ec70e6bd39a0e65b8b9e6a1c743bc790909b
johnlloyd <yukimiranda26@gmail.com>
2023-10-27 22:28:45 +0800

    all recomendation is done

--- src/app/manage-sales/manage-sales.component.ts
+++ src/app/manage-sales/manage-sales.component.ts
@@ -1,4 +1,4 @@
-import { formatDate } from '@angular/common';
+import { DatePipe, formatDate } from '@angular/common';
 import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Database, onValue, ref, set } from '@angular/fire/database';
@@ -28,7 +28,7 @@ export class ManageSalesComponent implements OnInit {
 
 
   constructor(public router:Router,public database:Database
-    ,private FireDb: AngularFireDatabase) {
+    ,private FireDb: AngularFireDatabase,private datePipe: DatePipe) {
       this.logs = FireDb.list('/logs').valueChanges();
      
     const sessionValue = sessionStorage.getItem('type');
@@ -245,4 +245,99 @@ this.transac = ad.sales
    }, 1000); // Adjust the delay time as needed
   } 
 }
+
+startday = formatDate(new Date(), 'MM~dd~yyyy', 'en')
+endday = formatDate(new Date(), 'MM~dd~yyyy', 'en')
+
+ondatestart() {
+  return formatDate(this.startday, 'MM~dd~yyyy', 'en');
+}
+ondateend() {
+  return formatDate(this.endday, 'MM~dd~yyyy', 'en');
+}
+
+looper() {
+  this.totalsales = 0
+  this.totaltransac = 0
+ this.totalgcash = 0
+ this.totalcash = 0
+ this.gcount = 0
+ this.ccount = 0
+ this.ctotalsales = 0
+ this.ctotaltransac = 0
+ this.ctotalgcash = 0
+ this.ctotalcash = 0
+ this.cgcount = 0
+this.cccount = 0
+  // Parse startday and endday into JavaScript Date objects
+  const startDate = new Date(this.startday);
+  const endDate = new Date(this.endday);
+
+  // Use a for loop to iterate over the dates between startday and endday
+  for (let currentDate = startDate; currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
+    // Do something with the current date (for example, log it)
+    let day = formatDate(currentDate, 'MM~dd~yyyy', 'en')
+    console.log(day)
+    const starCountRefclient = ref(this.database, '/sales/daily/' + day);
+
+    onValue(starCountRefclient, (snapshot) => {
+     const cd = snapshot.val();  
+
+     this.ctotaltransac = cd.sales + this.totaltransac;
+     this.ctotalsales = cd.income + this.totalsales;
+     this.ctotalgcash = cd.gsales +  this.totalgcash;
+     this.ctotalcash = cd.gsales + this.totalcash;
+     this.cgcount = cd.gcash + this.gcount;
+     this.cccount = cd.cash + this.ccount;
+    
+     }); 
+     if (this.ctotalsales === undefined || Number.isNaN(this.ctotalsales) ) {
+    
+    } else {
+      this.totalsales = this.ctotalsales
+    }
+
+    if (this.ctotaltransac === undefined || Number.isNaN(this.ctotaltransac)) {
+     
+    } else {
+      this.totaltransac = this.ctotaltransac
+    }
+
+    if (this.ctotalgcash === undefined || Number.isNaN(this.ctotalgcash)) {
+     
+    } else {
+      this.totalgcash = this.ctotalgcash
+    }
+
+    if (this.ctotalcash === undefined || Number.isNaN(this.ctotalcash)) {
+    
+    } else {
+      this.totalcash = this.ctotalcash
+    }
+
+    if (this.cgcount === undefined || Number.isNaN(this.cgcount)) {
+     
+    } else {
+      this.gcount = this.cgcount
+    }
+
+    if (this.cccount === undefined || Number.isNaN(this.cccount)) {
+    
+    } else {
+      this.ccount = this.cccount
+    }
+  }
+}
+totalsales = 0
+totaltransac = 0
+totalgcash = 0
+totalcash = 0
+gcount = 0
+ccount = 0
+ctotalsales = 0
+ctotaltransac = 0
+ctotalgcash = 0
+ctotalcash = 0
+cgcount = 0
+cccount = 0
 }
